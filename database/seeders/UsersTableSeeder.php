fab492127776c628cc7b17c8b512acae0deaccc3
Syed Nazir Hussain <nazir.hussain@branex.com>
2024-07-11 12:58:10 +0500

    fix user seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-
 
 class UsersTableSeeder extends Seeder
 {
@@ -16,23 +16,29 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-                'username' => 'alice',
-                'password' => Hash::make('password'),
+                'name' => 'Alice',
                 'email' => 'alice@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('hashed_pw1'),
+                'remember_token' => Str::random(10),
                 'created_at' => '2024-06-01 10:00:00',
                 'updated_at' => '2024-06-15 12:00:00',
             ],
             [
-                'username' => 'bob',
-                'password' => Hash::make('password'),
+                'name' => 'Bob',
                 'email' => 'bob@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('hashed_pw2'),
+                'remember_token' => Str::random(10),
                 'created_at' => '2024-06-05 11:00:00',
                 'updated_at' => '2024-06-18 13:00:00',
             ],
             [
-                'username' => 'charlie',
-                'password' => Hash::make('password'),
+                'name' => 'Charlie',
                 'email' => 'charlie@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('hashed_pw3'),
+                'remember_token' => Str::random(10),
                 'created_at' => '2024-06-10 12:00:00',
                 'updated_at' => '2024-06-20 14:00:00',
             ],
